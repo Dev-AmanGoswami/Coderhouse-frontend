504918958e4b8852e4efe7a9fa529e8b30756f0f
Dev-AmanGoswami <aman.goswami.dev@gmail.com>
2024-12-11 23:54:35 +0530

    Implementing: Login and Registration

--- src/pages/Steps/StepPhoneEmail/StepPhoneEmail.jsx
+++ src/pages/Steps/StepPhoneEmail/StepPhoneEmail.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-const StepPhoneEmail = () => {
+const StepPhoneEmail = ({ onNext }) => {
     return(
-        <div>StepPhoneEmail Page</div>
+        <>
+            <div>StepPhoneEmail Page</div>
+            <button onClick={onNext}>Next</button>
+        </>
     )
 }
 
