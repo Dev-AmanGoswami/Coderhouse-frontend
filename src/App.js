90e5d372b8bb7bb5ce8ccb25ce1c5dff6419aba8
Dev-AmanGoswami <aman.goswami.dev@gmail.com>
2024-12-13 00:39:18 +0530

    Implementing: Protected routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Outlet, Navigate} from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Navigation from './components/global/navigation/Navigation';
-import Register from './pages/Register/Register';
-import Login from './pages/Login/Login';
+import Authenticate from './pages/Authenticate/Authenticate';
+import Activate from './pages/Activate/Activate';
+import Rooms from './pages/Rooms/Rooms';
+const isAuth = true;
+const user = {
+  activated: true
+}
 
 function App() {
   return (
     <BrowserRouter>
       <Navigation />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route element={<GuestRoute />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/authenticate" element={<Authenticate />} />
+        </Route>
+        <Route element={<SemiProtectedRoute />}>
+          <Route path="/activate" element={<Activate />} />
+        </Route>
+        <Route element={<ProtectedRoute />}>
+          <Route path="/rooms" element={<Rooms />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
+}
+
+// Guest route is also a route component all route checks will be implemented here
+const GuestRoute = () => {
+  console.log("Aman");
+  return isAuth ? <Navigate to="/rooms" /> : <Outlet />
+}
+
+const SemiProtectedRoute = () => {
+  return !isAuth ? <Navigate to="/" /> : (isAuth && !user.activated) ? <Outlet /> : <Navigate to="/rooms" />;
+}
+
+const ProtectedRoute = () => {
+  return !isAuth ? <Navigate to="/" /> : (isAuth && !user.activated) ? <Navigate to="/activate" /> : <Outlet />;
 }
 
 export default App;
